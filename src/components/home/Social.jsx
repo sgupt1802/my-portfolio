09b0b1d9d5124867a6d1505567644f4c0aac91c1
sgupt1802 <sgupta_b21@it.vjti.ac.in>
2024-06-04 23:04:44 +0530

    initial commit

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Social = () => {
+  return (
+    <div className="home__social">
+        <a href="#insta" className="home__social-icon" target="_blank">
+        <i class="uil uil-instagram"></i>
+        </a>
+
+        <a href="#dribble" className="home__social-icon" target="_blank">
+        <i class="uil uil-dribble"></i>
+        </a>
+
+        <a href="#github" className="home__social-icon" target="_blank">
+        <i class="uil uil-github-alt"></i>
+        </a>
+    </div>
+  )
+}
+
+export default Social
