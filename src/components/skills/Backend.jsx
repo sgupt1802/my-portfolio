ef4c76004aa8ad441b68b442e7c1e833fea0ba00
sgupt1802 <sgupta_b21@it.vjti.ac.in>
2024-06-22 23:41:52 +0530

    about_section_change

--- src/components/skills/Backend.jsx
+++ src/components/skills/Backend.jsx
@@ -43,6 +43,16 @@ const Backend = () => {
                 </div>
             </div>
         </div>
+
+        <div className="skills__group">
+            <div className="skills__data">
+                <i class="bx bx-badge-check"></i>
+                <div>
+                    <h3 className="skills__name">Express</h3>
+                    <span className="skills__level">Intermediate</span>
+                </div>
+            </div>
+        </div>
     </div>
 
 </div>
