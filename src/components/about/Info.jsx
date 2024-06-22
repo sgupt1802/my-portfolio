53fc58523f4548034a300d7b3953b575190278b2
sgupt1802 <sgupta_b21@it.vjti.ac.in>
2024-06-22 23:30:19 +0530

    about_section_change

--- src/components/about/Info.jsx
+++ src/components/about/Info.jsx
@@ -11,6 +11,8 @@ const Info = () => {
         <span className="about__subtitle">1 year working</span>
       </div>
 
+      
+
       <div className="about__box">
         <i class="bx bx-briefcase-alt about__icon"></i>
         <h3 className="about__title">Completed</h3>
