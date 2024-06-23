c40aeace93090b7ff3408c3acf14d18daff71df9
sgupt1802 <sgupta_b21@it.vjti.ac.in>
2024-06-23 23:59:52 +0530

    services_change

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -114,7 +114,7 @@ const Services = () => {
                             <li className="services__modal-service">
                                 <i className="uil uil-check-circle services__modal-icon"></i>
                                 <p className="services__modal-info">
-                                    Backend Handling
+                                    React 
                                 </p>
                             </li>
                         </ul>
