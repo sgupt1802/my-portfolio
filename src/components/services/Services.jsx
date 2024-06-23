0de135674e248b64db4e368c71ef51be9d5df72a
sgupt1802 <sgupta_b21@it.vjti.ac.in>
2024-06-23 23:59:09 +0530

    qual_change

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -114,7 +114,7 @@ const Services = () => {
                             <li className="services__modal-service">
                                 <i className="uil uil-check-circle services__modal-icon"></i>
                                 <p className="services__modal-info">
-                                    Design and mockups of products for companies
+                                    Backend Handling
                                 </p>
                             </li>
                         </ul>
