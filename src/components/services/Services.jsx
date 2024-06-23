19d02aef2c4ffea2d5d6173477796d6c7c2d8fac
sgupt1802 <sgupta_b21@it.vjti.ac.in>
2024-06-23 23:54:14 +0530

    services_change

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -18,7 +18,7 @@ const Services = () => {
                 <div>
                     <i className="uil uil-web-grid services__icon"></i>
                     <h3 className="services__title">
-                        Product <br/> Designer
+                        Web <br/> Designer
                     </h3>
                 </div>
                 <span className="services__button" onClick={()=> toggleTab(1)}>View More<i className="uil uil-arrow-right services__button-icon"></i></span>
@@ -27,7 +27,7 @@ const Services = () => {
                     <div className="services__modal-content">
                         <i onClick={()=> toggleTab(0)} className="uil uil-times services__modal-close"></i>
                         <h3 className="services__modal-title">Product Designer</h3>
-                        <p className="services__modal-description">Service with more that 3 years of experience</p>
+                        <p className="services__modal-description">Service with more that 2months of experience</p>
 
                         <ul className="services__modal-services grid">
                             <li className="services__modal-service">
@@ -72,15 +72,15 @@ const Services = () => {
             <div className="services__content">
                 <div>
                     <i className="uil uil-arrow services__icon"></i>
-                    <h3 className="services__title">UI/UX <br /> Designer</h3>
+                    <h3 className="services__title">Web <br /> Development</h3>
                 </div>
                 <span onClick={()=> toggleTab(2)} className="services__button">View More<i className="uil uil-arrow-right services__button-icon"></i></span>
 
                 <div className={toggleState === 2 ? "services__modal active-modal" : "services__modal"}>
                     <div className="services__modal-content">
                         <i onClick={()=> toggleTab(0)} className="uil uil-times services__modal-close"></i>
-                        <h3 className="services__modal-title">UI/UX Designer</h3>
-                        <p className="services__modal-description">Service with more that 3 years of experience</p>
+                        <h3 className="services__modal-title">Web Application Developer</h3>
+                        <p className="services__modal-description">Service with more that 2 months of experience</p>
 
                         <ul className="services__modal-services grid">
                             <li className="services__modal-service">
